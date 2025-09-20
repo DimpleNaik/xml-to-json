75c8adf86a64514ca8b7aa534164cbdde0e3089d
Dimple Naik <dimple.naik2003@gmail.com>
2025-09-20 13:15:07 +0530

    ADD Procfile for Heroku deployment

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ from fastapi import FastAPI, UploadFile, File, HTTPException
 from xml_parser import xml_to_json
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
+import os
+import uvicorn
+
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # allow your React dev server origin
+    allow_origins=["http://localhost:3000"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -25,3 +28,8 @@ async def xml_to_json_endpoint(file: UploadFile = File(...)):
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"XML parsing error: {str(e)}")
     return JSONResponse(content=json_dict)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+
